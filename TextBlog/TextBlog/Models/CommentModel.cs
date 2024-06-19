d63737c343d2249194744518275419c8d19240fa
nemajor1 <kolaivanov040@gmail.com>
2024-06-19 22:21:55 +0300

    Add models for DB and add BlogContext.cs

---
+++ TextBlog/TextBlog/Models/CommentModel.cs
@@ -0,0 +1,11 @@
+﻿public class CommentModel
+{
+    public int commentId { get; set; }
+    public int postId { get; set; }
+    public int authorId { get; set; }
+    public string commentContent { get; set; }
+    public DateTime commentDate { get; set; }
+
+    public UserModel Author { get; set; }
+    public PostModel Post { get; set; }
+}
